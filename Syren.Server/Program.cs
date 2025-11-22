cd64036d8694a900a64503c2225c68128ca692b2
PringlesGang <liamelderbroek123@gmail.com>
2025-11-22 18:29:21 +0100

    Removed SignalR dependency

--- Syren.Server/Program.cs
+++ Syren.Server/Program.cs
@@ -1,18 +1,14 @@
 ﻿using Syren.Server.Extensions;
-using Syren.Server.Hubs;
 using Syren.Server.Services;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddLogging(options => options.AddConsole());
 builder.Services.AddSingleton<IDistanceService, DistanceService>();
-builder.Services.AddSignalR();
 builder.Services.AddMqttServices(builder.Configuration);
 
 var app = builder.Build();
 
 var distanceService = app.Services.GetRequiredService<IDistanceService>();
-
-app.MapHub<DistanceHub>("/distance");
 
 app.Run();
